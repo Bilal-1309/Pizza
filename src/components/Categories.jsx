d8db59ccd18a8c42db9180824fd8b52f34fc7785
Bilal <obkh1309@mail.ru>
2022-03-09 12:05:55 +0300

    работа над контекст меню

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -12,7 +12,7 @@ const Categories = ({ items, onClickItem}) => {
             <ul>
                 <li className={activeItem === null ? 'active' : ''}
                     onClick={()=> onSelectItem(null)}>Все</li>
-                {
+                {items &&
                     items.map((name, index)=>
                         <li className={activeItem === index ? 'active' : ''}
                             onClick={()=> onSelectItem(index)}
